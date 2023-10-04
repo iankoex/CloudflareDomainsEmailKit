e66eea43db1a43cf732ec4bedb668c679dc29ed0
Iankoex <koexian@gmail.com>
2023-10-04 03:11:53 +0300

    nio core to get byte buffer

--- Sources/CloudflareDomainsEmailKit/EmailClient.swift
+++ Sources/CloudflareDomainsEmailKit/EmailClient.swift
@@ -1,5 +1,6 @@
 import Foundation
 import AsyncHTTPClient
+import NIOCore
 
 public struct EmailClient {
     
@@ -11,7 +12,7 @@ public struct EmailClient {
             var request = HTTPClientRequest(url: workerURL)
             request.method = .POST
             request.headers.add(name: "content-type", value: "application/json")
-            request.body = .bytes(.init(data: data))
+            request.body = .bytes(ByteBuffer(data: data))
             let response = try await httpClient.execute(request, timeout: .seconds(60))
             guard response.status == .ok else {
                 throw EmailClientError.somethingWentWrong
