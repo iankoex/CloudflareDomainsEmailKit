7a11a3c26999f4d7400f9ffa46bcb369dc863437
Iankoex <koexian@gmail.com>
2023-10-06 08:07:25 +0300

    platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "CloudflareDomainsEmailKit",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
